57676b822d96b90fbc189ddb64c01b6034a5ff8f
Benjamin Heinle <bheinle7@gmail.com>
2017-06-15 19:25:26 -0400

    initial commit of site

---
+++ main.js
@@ -0,0 +1,14 @@
+var navAbout = document.getElementById("navAbout")
+navAbout.onmouseover = function(){
+  navAbout.style.color = "black"
+};
+navAbout.onmouseout = function(){
+  navAbout.style.color = "white"
+};
+var navPortfolio = document.getElementById("navPortfolio")
+navPortfolio.onmouseover = function(){
+  navPortfolio.style.color = "black"
+};
+navPortfolio.onmouseout = function(){
+  navPortfolio.style.color = "white"
+};
